5fb94076e081bd0d44b34bdd6f8ff2598d97c8d7
Max02140 <Max02140@users.noreply.github.com>
2026-05-15 18:41:52 +0200

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+         /**
+     * @param {string} s
+     * @return {boolean}
+     */
+
+        while (s.includes('()') || s.includes('{}') || s.includes('[]')) {
+            s = s.replace('()', '');
+            s = s.replace('{}', '');
+            s = s.replace('[]', '');
+        }
+        return s === '';
+    }
+}
